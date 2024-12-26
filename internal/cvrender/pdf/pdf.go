ed5cec85d7b6fecb7e81e7faa4ba74eca78909d6
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2024-12-26 23:42:45 +0100

    fix: Better manage error on starting local temp server

--- internal/cvrender/pdf/pdf.go
+++ internal/cvrender/pdf/pdf.go
@@ -23,14 +23,22 @@ func RenderFormatPDF(cv model.CV, outputDirectory string, inputFilename string, 
 	utils.CheckError(err)
 	defer w.Close()
 
+	localServerUrl := fmt.Sprintf("http://localhost:%d", utils.CliArgs.Port)
+
 	// Run the server to output the HTML
+	logrus.Info("Starting a temporary server at address ", localServerUrl)
 	go func() {
 		cvserve.StartServer(outputDirectory)
 
 	}()
-	localServerUrl := fmt.Sprintf("http://localhost:%d", utils.CliArgs.Port)
 	// Open the browser and convert the page to PDF
-	rod.New().MustConnect().MustPage(localServerUrl).MustWaitLoad().MustPDF(outputFilePath)
+	err = rod.Try(func() {
+		rod.New().MustConnect().MustPage(localServerUrl).MustWaitLoad().MustPDF(outputFilePath)
+	})
+	if err != nil {
+		message := fmt.Sprintf("ERROR: Failed to connect to the server %s", localServerUrl)
+		logrus.Fatal(message)
+	}
 
 	return nil
 }
