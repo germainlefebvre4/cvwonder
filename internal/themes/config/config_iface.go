dbf033b0a77eabbcc2e9b835b42001d00d1c4fda
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2025-04-17 16:24:09 +0200

    test: Add missing mocks from mockery

--- internal/themes/config/config_iface.go
+++ internal/themes/config/config_iface.go
@@ -1,13 +1,5 @@
 package theme_config
 
-type ThemeConfig struct {
-	Name           string `yaml:"name"`
-	Slug           string `yaml:"slug"`
-	Description    string `yaml:"description"`
-	Author         string `yaml:"author"`
-	MinimumVersion string `yaml:"minimumVersion"`
-}
-
 type ThemeConfigInterface interface {
 	GetThemeConfigFromURL(githubRepo GithubRepo) ThemeConfig
 	GetThemeConfigFromDir(dir string) ThemeConfig
